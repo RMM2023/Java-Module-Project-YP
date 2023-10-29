5a18d0d7cb8210c708f4daf7f7d3032181eea708
RMM2023 <rumuzi1991@gmail.com>
2023-10-29 10:38:09 +0300

    Check fix

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -3,30 +3,36 @@ package src.main.java;
 import java.util.ArrayList;
 import java.util.List;
 
-class Calculate{
+class Calculate {
     private List<Product> list = new ArrayList<>();
     private int people;
-    public Calculate(int people){
-        this.people=people;
+
+    public Calculate(int people) {
+        this.people = people;
     }
-    public void addProduct(String name, double price){
+
+    public void addProduct(String name, double price) {
         Product newProduct = new Product(name, price);
         list.add(newProduct);
     }
+
     public double getTotal() {
         double total = 0;
-        for(Product i: list){
+        for (Product i : list) {
             total = total + i.getPrice();
         }
         return total;
     }
-    public void printCheck(){
+
+    public void printCheck() {
         System.out.println("Чек:");
-        for (Product i:list){
-            System.out.println(i.getName());
+        for (Product i : list) {
+            System.out.println(i.getName() + " - " + Formatter.formatRuble(i.getPrice()));
         }
-        double personPrice = getTotal()/people;
+
+        double total = getTotal();
+        double personPrice = total / people;
         String ruble = Formatter.formatRuble(personPrice);
-        System.out.println("С каждого: " + personPrice + ruble);
+        System.out.println("С каждого: " + ruble);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,27 +2,41 @@ package src.main.java;
 
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int people;
-        while (true){
+        while (true) {
             System.out.println("Введите количество гостей ");
-            people = scanner.nextInt();
-            if (people<=1){
-                System.out.println("Не корректное значение");
-            }else{
-                break;
+            if (scanner.hasNextInt()) {
+                people = scanner.nextInt();
+                if (people <= 1) {
+                    System.out.println("Не корректное значение");
+                } else {
+                    break;
+                }
+            } else {
+                System.out.println("Вы ввели не число. Попробуйте еще раз.");
+                scanner.next();
             }
         }
+
         Calculate calculate = new Calculate(people);
-        while (true){
+        while (true) {
             System.out.println("Введите название товара:");
             String name = scanner.next();
             System.out.println("Введите цену товара:");
-            double price = scanner.nextDouble();
-            calculate.addProduct(name, price);
-            System.out.println("Добавить еще товара? \nВведите Завершить для выхода");
+            if (scanner.hasNextDouble()) {
+                double price = scanner.nextDouble();
+                calculate.addProduct(name, price);
+            } else {
+                System.out.println("Вы ввели не число. Попробуйте еще раз.");
+                scanner.next();
+                continue;
+            }
+
+            System.out.println("Добавить еще товара?");
+            System.out.println("Введите Завершить для выхода");
             String input = scanner.next();
             if (input.equalsIgnoreCase("Завершить"))
                 break;
